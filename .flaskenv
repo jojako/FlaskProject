434ca56a49dc8b0967c34077bbb525871108b17e
Alexander Nyberg <alexandernyberg@me.com>
2022-01-18 11:37:36 +0100

    Added home.html and created app object

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 FLASK_APP=app
-FLASK_ENVIRONMENT=development
+FLASK_ENV=development
--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
-def main():
-    pass
+from venv import create
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__, template_folder='./templates')
+
+    return app
 
 
 if __name__ == '__main__':
-    main()
+    create_app().run()
