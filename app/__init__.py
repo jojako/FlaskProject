d988e2ebaf04a0378bb866ef8003de29525eaebc
Alexander Nyberg <alexandernyberg@me.com>
2022-01-18 16:59:25 +0100

    Set color palette of main page

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
-from venv import create
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__, template_folder='./templates')
+
+    from app.blueprints.open import bp_open
+    app.register_blueprint(bp_open)
 
     return app
 
