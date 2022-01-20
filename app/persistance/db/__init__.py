2e116ff9087cf9fd68e8121dd69f0857470cf31f
Alexander Nyberg <alexandernyberg@me.com>
2022-01-20 10:27:14 +0100

    Created init_db function

--- app/persistance/db/__init__.py
+++ app/persistance/db/__init__.py
@@ -5,9 +5,13 @@ db = client.login_db
 
 
 def init_db(app):
+
+    global client, db
+
     username = app.config['DB_USER']
     password = app.config['DB_PASSWORD']
     host = app.config['DB_HOST']
     port = app.config['DB_PORT']
     database = app.config['DB_NAME']
-
+    client = MongoClient(f'mongodb://{username}:{password}@{host}:{port}')
+    db = client[database]
