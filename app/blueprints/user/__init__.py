50d2d6e800c5a8e7fb5c03b67add516634ddb513
Johannes Kopeczky <johannes87@gmail.com>
2022-01-21 14:25:23 +0100

    Added @loginrequired

--- app/blueprints/user/__init__.py
+++ app/blueprints/user/__init__.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 ## TODO: Flask Login needs to make sure you're authenticated
 
 bp_user = Blueprint('bp_user', __name__)
 
 
 @bp_user.get('/account')
+@login_required
 def profile():
     render_template('profile.html')
