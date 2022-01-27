dc9dfd605af36f7576535cb1095fff64251774e3
Alexander Nyberg <alexandernyberg@me.com>
2022-01-27 15:21:41 +0100

    Added sub-menu for admin page

--- app/blueprints/admin/__init__.py
+++ app/blueprints/admin/__init__.py
@@ -3,9 +3,10 @@ from flask_login import current_user
 
 bp_admin = Blueprint('bp_admin', __name__)
 
+
 @bp_admin.before_request
 def before_request():
-    if not current_user.is_authenticated or not current_user.accesslevel == 'admin':
+    if not current_user.is_authenticated or not current_user.accesslevel('admin'):
         return redirect(url_for('bp_open.index'))
 
 
